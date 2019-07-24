9c8c0ba1e5ae02eeac531d54d3507589c7d7ee7c
shahbaz272 <shahbaz272@hotmail.com>
2019-07-24 15:42:49 -0700

    Update 1520094343_sql_project.sql

--- 1520094343_sql_project.sql
+++ 1520094343_sql_project.sql
@@ -24,7 +24,6 @@ exploring the data, and getting acquainted with the 3 tables. */
 
 
 
-
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
 
@@ -38,11 +37,16 @@ WHERE membercost > 0;
 "Massage Room 2","9.9"
 "Squash Court","3.5"
 
+
 /* Q2: How many facilities do not charge a fee to members? */
 
 SELECT COUNT( * ) AS NoFeeFacilityCount
 FROM  `Facilities` 
 WHERE membercost = 0;
+
+"NoFeeFacilityCount"
+"4"
+
 
 /* Q3: How can you produce a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost?
@@ -53,6 +57,18 @@ SELECT facid, name AS  'Facility Name', membercost, monthlymaintenance
 FROM  `Facilities` 
 WHERE membercost / monthlymaintenance < 0.2;
 
+"facid","Facility Name","membercost","monthlymaintenance"
+"0","Tennis Court 1","5.0","200"
+"1","Tennis Court 2","5.0","200"
+"2","Badminton Court","0.0","50"
+"3","Table Tennis","0.0","10"
+"4","Massage Room 1","9.9","3000"
+"5","Massage Room 2","9.9","3000"
+"6","Squash Court","3.5","80"
+"7","Snooker Table","0.0","15"
+"8","Pool Table","0.0","15"
+
+
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
 
@@ -60,6 +76,11 @@ SELECT *
 FROM Facilities
 WHERE facid
 IN ( 1, 5 );
+
+"facid","name","membercost","guestcost","initialoutlay","monthlymaintenance"
+"1","Tennis Court 2","5.0","25.0","8000","200"
+"5","Massage Room 2","9.9","80.0","4000","3000"
+
 
 /* Q5: How can you produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
@@ -73,6 +94,18 @@ SELECT name, monthlymaintenance,
 	END AS  'CheapOrExpensive'
 FROM  `Facilities`;
 
+"name","monthlymaintenance","CheapOrExpensive"
+"Tennis Court 1","200","expensive"
+"Tennis Court 2","200","expensive"
+"Badminton Court","50","cheap"
+"Table Tennis","10","cheap"
+"Massage Room 1","3000","expensive"
+"Massage Room 2","3000","expensive"
+"Squash Court","80","cheap"
+"Snooker Table","15","cheap"
+"Pool Table","15","cheap"
+
+
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Do not use the LIMIT clause for your solution. */
 
@@ -81,6 +114,9 @@ FROM  `Members`
 WHERE joindate = ( 
 SELECT MAX( joindate ) 
 FROM  `Members` );
+
+"firstname","surname"
+"Darren","Smith"
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
 Include in your output the name of the court, and the name of the member
@@ -95,6 +131,54 @@ WHERE F.name LIKE (
 'TENNIS COURT%'
 )
 ORDER BY 2;
+
+"FaciltiyName","FULLNAME"
+"Tennis Court 1","Anne Baker"
+"Tennis Court 2","Anne Baker"
+"Tennis Court 2","Burton Tracy"
+"Tennis Court 1","Burton Tracy"
+"Tennis Court 1","Charles Owen"
+"Tennis Court 2","Charles Owen"
+"Tennis Court 2","Darren Smith"
+"Tennis Court 1","David Farrell"
+"Tennis Court 2","David Farrell"
+"Tennis Court 1","David Jones"
+"Tennis Court 2","David Jones"
+"Tennis Court 1","David Pinker"
+"Tennis Court 1","Douglas Jones"
+"Tennis Court 1","Erica Crumpet"
+"Tennis Court 1","Florence Bader"
+"Tennis Court 2","Florence Bader"
+"Tennis Court 2","Gerald Butters"
+"Tennis Court 1","Gerald Butters"
+"Tennis Court 2","GUEST GUEST"
+"Tennis Court 1","GUEST GUEST"
+"Tennis Court 2","Henrietta Rumney"
+"Tennis Court 2","Jack Smith"
+"Tennis Court 1","Jack Smith"
+"Tennis Court 2","Janice Joplette"
+"Tennis Court 1","Janice Joplette"
+"Tennis Court 2","Jemima Farrell"
+"Tennis Court 1","Jemima Farrell"
+"Tennis Court 1","Joan Coplin"
+"Tennis Court 2","John Hunt"
+"Tennis Court 1","John Hunt"
+"Tennis Court 1","Matthew Genting"
+"Tennis Court 2","Millicent Purview"
+"Tennis Court 2","Nancy Dare"
+"Tennis Court 1","Nancy Dare"
+"Tennis Court 1","Ponder Stibbons"
+"Tennis Court 2","Ponder Stibbons"
+"Tennis Court 2","Ramnaresh Sarwin"
+"Tennis Court 1","Ramnaresh Sarwin"
+"Tennis Court 1","Tim Boothe"
+"Tennis Court 2","Tim Boothe"
+"Tennis Court 2","Tim Rownam"
+"Tennis Court 1","Tim Rownam"
+"Tennis Court 2","Timothy Baker"
+"Tennis Court 1","Timothy Baker"
+"Tennis Court 2","Tracy Smith"
+"Tennis Court 1","Tracy Smith"
 
 
 /* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
@@ -117,6 +201,22 @@ BETWEEN  '2012-09-14'
 AND  '2012-09-15'
 HAVING Cost >30
 ORDER BY 3 DESC;
+
+
+"FaciltiyName","FULLNAME","Cost"
+"Massage Room 2","GUEST GUEST","320.0"
+"Massage Room 1","GUEST GUEST","160.0"
+"Massage Room 1","GUEST GUEST","160.0"
+"Massage Room 1","GUEST GUEST","160.0"
+"Tennis Court 2","GUEST GUEST","150.0"
+"Tennis Court 1","GUEST GUEST","75.0"
+"Tennis Court 1","GUEST GUEST","75.0"
+"Tennis Court 2","GUEST GUEST","75.0"
+"Squash Court","GUEST GUEST","70.0"
+"Massage Room 1","Jemima Farrell","39.6"
+"Squash Court","GUEST GUEST","35.0"
+"Squash Court","GUEST GUEST","35.0"
+
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
@@ -141,6 +241,19 @@ END
 ) >30
 ORDER BY 3 DESC;
 
+"FaciltiyName","FULLNAME","Cost"
+"Massage Room 2","GUEST GUEST","320.0"
+"Massage Room 1","GUEST GUEST","160.0"
+"Massage Room 1","GUEST GUEST","160.0"
+"Massage Room 1","GUEST GUEST","160.0"
+"Tennis Court 2","GUEST GUEST","150.0"
+"Tennis Court 1","GUEST GUEST","75.0"
+"Tennis Court 2","GUEST GUEST","75.0"
+"Tennis Court 1","GUEST GUEST","75.0"
+"Squash Court","GUEST GUEST","70.0"
+"Massage Room 1","Jemima Farrell","39.6"
+"Squash Court","GUEST GUEST","35.0"
+"Squash Court","GUEST GUEST","35.0"
 
 
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
@@ -158,3 +271,10 @@ INNER JOIN  `Members` AS M ON M.memid = B.memid
 GROUP BY 1 
 HAVING REVENUE <1000
 ORDER BY 2 DESC;
+
+
+"FaciltiyName","REVENUE"
+"Pool Table","270.0"
+"Snooker Table","240.0"
+"Table Tennis","180.0"
+
